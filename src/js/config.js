1dc6c2bc4fe830e7751f17831c08adb1ce4af4ad
Chiragtyagi03 <chiragtyagi499@gmail.com>
2021-12-08 13:38:57 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9e7ba24e-6cda-4995-853e-2b0d089538f0';
+export const MODAL_CLOSE_SEC = 2.5;
